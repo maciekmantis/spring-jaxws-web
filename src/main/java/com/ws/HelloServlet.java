ce415f07db143754e9ce445af3c93ce27ba3668c
maciekmantis <maciej.scislowski@gmail.com>
2014-03-20 20:46:13 +0100

    Initial commit.

---
+++ src/main/java/com/ws/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.ws;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ws.client.HelloWorldWSService;
+import com.ws.client.HelloWorldWS;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+		HelloWorldWSService service = new HelloWorldWSService();
+		HelloWorldWS port= service.getHelloWorldWSPort();
+	    out.println(port.getHelloWorld());
+	}
+
+}
